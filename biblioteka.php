6ddd83978a3358ddb8f6c86ee034a28e5b83cab9
Dominik Żwirski <dominik.zwirski@teb.edu.pl>
2024-10-09 11:52:47 +0200

    html 70% css 85% php 0%

---
+++ biblioteka.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteka publiczna</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+<div id="left">
+    <h3>Polecam dzieła aktorów</h3>
+    <ol>
+        <li>element1</li>
+        <li>element2</li>
+        <li>element3</li>
+    </ol>
+</div>
+<div id="center">
+<h3>ul. Czytelnicza 25, Książkowice&nbsp;Wielkie</h3>
+<a href="ekretariat@biblioteka.pl"><p>napisz do nas</p></a>
+<img src="biblioteka.png" alt="książki">
+</div>
+<div id="right">
+    <div id="top">
+        <h3>dodaj czytelnika</h3>
+        <form action="biblioteka.php"method='post'>
+            <label for="firstName"></label>
+            <input type="text">
+    </div>
+        <div id="bottom"></div>
+
+    </main>
+    <footer>
+numer pesel;0000000000
+
+    </footer>
+</body>
+</html>
